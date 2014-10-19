02ac5eb14412e41ed5a8ba8bdb778024ebd18c2b
Alex Chao <alexchao56@gmail.com>
2014-10-18 18:26:57 -0700

    demo page

--- server/js/main.js
+++ server/js/main.js
@@ -3,17 +3,32 @@ var shortURL = "";
 
 $(function() {
 
-    // hide the confirmation webapge from view
+  // hide the confirmation page from view
   $("#confirmation").hide();
 
   // ---- event handler ---- //
-  $("#submit").click(function(e) {
+  $("#generateBlomo").click(function(e) {
     console.log("Do I ever get here?");
     longURL = $("#long-URL").val();
     shortURL = $("#short-URL").val();
     console.log("Long URL is: " + longURL);
     console.log("Short URL is: " + shortURL);
+    $(".shortenedURL").empty().append(shortURL);
+
+    // Show the confirmation page
+    $("#confirmation").show();
+
+    // Hide the input form
+    $("#inputForm").hide();
+
+
   });
-    
+
+  $("#blomoAgain").click(function(e) {
+    $("#inputForm").show();   
+    $("#confirmation").hide();
+  });
+
 
 });
+
