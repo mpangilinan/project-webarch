88e32ad238bae36b0fe88ac9e9e35ac4246c2a72
Melissa <mpangilinan@ischool.berkeley.edu>
2014-12-09 18:00:11 -0800

    adjusted validation to alphanumeric shorteners

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -6,12 +6,9 @@ $(function() {
   // ---- event handler ---- //
   $("#generateBlomo").click(function(e) {
     
-
-
-
     longURL = $("#long-URL").val();
     shortURL = $("#short-URL").val();
-    letters = "[a-zA-Z]+";
+    letters = /^[0-9a-zA-Z]+$/;
     
     // Validate short URL is only letters
     console.log(shortURL);
@@ -24,13 +21,10 @@ $(function() {
        }
    } 
 
-
     if (longURL == null || longURL == "") {
       alert("Please enter a URL to blomo");
       return false;
     }
-
-
 
     console.log("Long URL is: " + longURL);
     console.log("Short URL is: " + shortURL);
